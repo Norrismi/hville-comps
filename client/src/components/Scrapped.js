cb423c98ea20148885480e5fff29cccca49313c5
Norrismi <Norrismi0@gmail.com>
2022-06-03 09:34:34 -0400

    added all endpoints

--- client/src/components/Scrapped.js
+++ client/src/components/Scrapped.js
@@ -2,33 +2,50 @@ import { useState, useEffect } from 'react'
 
 function Scrapped() {
 
-    const [data, setData] = useState(null);
+    const [price, setPrice] = useState(null);
+    const [sqft, setSqft] = useState(null);
+    const [address, setAddress] = useState(null);
+ 
+
 
     useEffect(() => {
-        fetch('/info')
+        fetch('/price')
             .then((res) => res.json())
-            .then((data) =>
-                setData(data)
-            )
+            .then((data) => setPrice(data))
+
+        fetch('/sqft')
+            .then((res) => res.json())
+            .then((data) => setSqft(data))
+
+        fetch('/address')
+            .then((res) => res.json())
+            .then((data) => setAddress(data))
 
     }, [])
 
 
-
-    console.log(data && data.sqft)
-
     return (
         <div>
-            <div>Scraped</div>
+
+            {price && price.price.map((item, i) => (
+                <ul key={item.id}>
+                    {`${i + 1}) ${item} `}
+                </ul>
+            ))}
 
 
-            
-      {data && data.price.map((item, i) => (
-          <ul >
-              {item}
-             {/* {`${i+1}) ${item.price} ${item.sqft}`} */}
-          </ul>
-      ))}
+            {sqft && sqft.sqft.map((item, i) => (
+                <ul key={item.id}>
+                    {item}
+
+                </ul>
+            ))}
+
+            {address && address.address.map((item, i) => (
+                <ul key={item.id}>
+                    {item}
+                </ul>
+            ))}
 
 
 
--- index.js
+++ index.js
@@ -4,12 +4,24 @@ const PORT = 5555;
 
 const price = require('./scrappedData/price.json')
 const sqft = require('./scrappedData/sqft.json')
-const propertyAddress = require('./scrappedData/address.json')
+const address = require('./scrappedData/address.json')
+
+
+app.get('/price', (req, res) => {
+    res.json({price})
+})
+
+app.get('/sqft', (req, res) => {
+    res.json({sqft})
+})
+
+app.get('/address', (req, res) => {
+    res.json({address})
+})
 
 
 app.get('/info', (req, res) => {
-    res.json( {price, sqft})
-
+    res.json({price, sqft})
 })
 
 
